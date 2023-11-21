a0b635fd6209f6bc5bb34495db2aae910c76701c
Avikaielef <AS226877@greendreamteam.onmicrosoft.com>
2023-11-21 17:39:14 +0200

    Added test for sender

--- Tests/test_sender.py
+++ Tests/test_sender.py
@@ -1,36 +1,44 @@
-# tests/test_sender.py
-
 from fastapi.testclient import TestClient
 from Sender import app
 
-client = TestClient(app)
-
 
 def test_calculate_sum():
-    response = client.post("/calculate/sum", json={"num1": 2, "num2": 3})
-    assert response.status_code == 200
-    assert response.json() == {"message": "Calculation request sent to the receiver."}
+    with TestClient(app) as client:
+        response = client.post("/calculate/sum?num1=2.5&num2=3.5")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Calculation request sent to the receiver."}
 
 
 def test_calculate_subtract():
-    response = client.post("/calculate/subtract", json={"num1": 5, "num2": 2})
-    assert response.status_code == 200
-    assert response.json() == {"message": "Calculation request sent to the receiver."}
+    with TestClient(app) as client:
+        response = client.post("/calculate/subtract?num1=5.5&num2=2.5")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Calculation request sent to the receiver."}
 
 
 def test_calculate_multiply():
-    response = client.post("/calculate/multiply", json={"num1": 4, "num2": 3})
-    assert response.status_code == 200
-    assert response.json() == {"message": "Calculation request sent to the receiver."}
+    with TestClient(app) as client:
+        response = client.post("/calculate/multiply?num1=2.5&num2=3.5")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Calculation request sent to the receiver."}
 
 
 def test_calculate_divide():
-    response = client.post("/calculate/divide", json={"num1": 10, "num2": 2})
-    assert response.status_code == 200
-    assert response.json() == {"message": "Calculation request sent to the receiver."}
+    with TestClient(app) as client:
+        response = client.post("/calculate/divide?num1=10&num2=2")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Calculation request sent to the receiver."}
 
 
 def test_calculate_invalid_operation():
-    response = client.post("/calculate/invalid_operation", json={"num1": 1, "num2": 2})
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Invalid operation"}
+    with TestClient(app) as client:
+        response = client.post("/calculate/invalid?num1=2.5&num2=3.5")
+        assert response.status_code == 400
+        assert "Invalid operation" in response.text
+
+
+def test_calculate_send_to_invalid_queue():
+    with TestClient(app) as client:
+        response = client.post("/calculate/invalid_operation?num1=2.5&num2=3.5")
+        assert response.status_code == 400
+        assert "Invalid operation" in response.text
